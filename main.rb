b82462d039bc9bbdb13da445a810780372655167
Dmitry Vyal <akamaus@gmail.com>
2013-05-13 13:42:02 +0400

    module main.rb, .gitignore added

---
+++ main.rb
@@ -0,0 +1,20 @@
+require 'pathname'
+
+load 'huge_diff.rb'
+
+case
+when ARGV.length != 2
+  $stderr.puts "Usage:\nruby main.rb <file1> <file2>"
+when !Pathname(ARGV[0]).file?
+  $stderr.puts "first argument must be a file"
+when !Pathname(ARGV[1]).file?
+  $stderr.puts "second argument must be a file"
+else
+  if ENV['PIECE_SIZE']
+    piece_size = ENV['PIECE_SIZE'].to_i
+  else
+    piece_size = 100000
+  end
+  hd = HugeDiff.new(File.new(ARGV[0]).lines.each, File.new(ARGV[1]).lines.each, piece_size)
+  hd.diff(Proc.new { |f| puts "#{f.strip} was deleted"}, Proc.new { |f| puts "#{f.strip} was created"})
+end
